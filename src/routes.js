839b5e9c0697e368e6fd4da12b7291449729ee14
Denny Azevedo <denny.filho@animaeducacao.com.br>
2024-03-05 21:44:01 -0300

    :bank: Ajustando a conexao com o Banco e ajustando rotas :vertical_traffic_light:

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 import { Router } from 'express'
+import User from './app/models/User'
+import { password } from './config/database'
 
 const routes = new Router()
 
 routes.get('/teste', (req, res) => res.json({ ok: true }))
+routes.get('/testedb', async (req, res) => {
+	const user = await User.create({
+		name: 'Denny Azevedo',
+		email: 'dennyazevedo@gmail.com',
+		password_hash: '123456',
+	})
+
+	return res.json(user)
+})
 
 export default routes
